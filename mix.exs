69916c047f4c316345023b0f545a7f92442b784d
Sean Moriarity <smoriarity.5@gmail.com>
2023-01-14 09:12:33 -0800

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule Pinecone.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
     ]
   end
 end
