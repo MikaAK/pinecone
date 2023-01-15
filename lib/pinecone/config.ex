d5ca6c878251b9cd1cb11ef8b3833b7e4a442ec1
Sean Moriarity <smoriarity.5@gmail.com>
2023-01-14 18:06:09 -0800

    Clean up some APIs

--- lib/pinecone/config.ex
+++ lib/pinecone/config.ex
@@ -4,4 +4,6 @@ defmodule Pinecone.Config do
   def api_key(), do: Application.get_env(:pinecone, :api_key)
 
   def environment(), do: Application.get_env(:pinecone, :environment)
+
+  def project_name(), do: Application.get_env(:pinecone, :project_name)
 end
