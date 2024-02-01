1824003d3f1c94fd8011acef80c9e85108b6120b
MikaAK <mika@kalathil.me>
2024-01-31 18:52:59 -0800

    chore: fix coverage config

--- mix.exs
+++ mix.exs
@@ -11,12 +11,17 @@ defmodule Pinecone.MixProject do
       deps: deps(),
       docs: docs(),
       package: package(),
+
+      test_coverage: [tool: ExCoveralls],
+
       preferred_cli_env: [
-        dialyzer: :test
+        dialyzer: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
       ],
-      elixirc_options: [
-        warnings_as_errors: true
-      ],
+
       dialyzer: [
         plt_add_apps: [:ex_unit, :mix, :credo],
         list_unused_filters: true,
